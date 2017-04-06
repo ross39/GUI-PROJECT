83d872fdab2b7f74c4199d856078ee1348b6dc3d
ross39 <Rheaney26@gmail.com>
2017-04-06 12:18:43 +0100

    Add files via upload

--- GUI PROJECT/Reaction.js
+++ GUI PROJECT/Reaction.js
@@ -1,4 +1,16 @@
-$(document).ready(function(e)){
+var imageArray = [];
+imageArray[0] = "./images/Rand/rand1.jpg";
+imageArray[1] = "./images/Rand/rand2.jpg";
+imageArray[2] = "./images/Rand/rand3.jpg";
+imageArray[3] = "./images/Rand/rand4.png";
+imageArray[3] = "./images/Rand/chip.jpg";
+function random()
+{
+	var r  = Math.floor(Math.random() * 4);
+	document.getElementById("active-slide").innerHTML =  "<img src = "+ imageArray[r] + " alt = image of pc parts class = slide title = Welcome to our E-commerce page id = first>";
+}
+
+/*$(document).ready(function(e)){
   $('.search-panel .dropdown-menu').find('a').click(function(e){
     e.preventDefault();
     var param = $(this).attr("href").replace("#","");
@@ -7,3 +19,4 @@ $(document).ready(function(e)){
     $('.input-group #search_param').val(param);
   });
 });
+*/
