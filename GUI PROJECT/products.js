5f0c5a1adbd73fdbb939d22690c82559377fe982
Ultan <ultankearns@gmail.com>
2017-04-26 18:32:58 +0100

    Updated products.js
    Added an object array to products.js which will make it easier to allow
    the cart to calculate the cost and quantity of individual items.

--- GUI PROJECT/products.js
+++ GUI PROJECT/products.js
@@ -2,6 +2,7 @@ var quantity = 0;
 var price = 0;
 var amount = 0;
 var previous = parseInt(sessionStorage.cost);
+var itemArr = new Array(17);
 if(previous  > 0)
 {
  	document.getElementById("cost").innerHTML = "<strong>Current amount in cart: €" + previous + "</strong>";
@@ -10,151 +11,51 @@ else
 {
 	document.getElementById("cost").innerHTML = "<strong>Current amount in cart: €0" + "</strong>";
 }
+function item(price,quan,totalQuan)
+{
+  this.price = price;
+  this.quan = quan;
+  this.totalQuan = totalQuan;
+}
+itemArr[0] = new item(450,0,0);
+itemArr[1] = new item(600,0,0);
+itemArr[2] = new item(500,0,0);
+itemArr[3] = new item(800,0,0);
+itemArr[4] = new item(250,0,0);
+itemArr[5] = new item(90,0,0);
+itemArr[6] = new item(80,0,0);
+itemArr[7] = new item(200,0,0);
+itemArr[8] = new item(180,0,0);
+itemArr[9] = new item(300,0,0);
+itemArr[10] = new item(350,0,0);
+itemArr[11] = new item(300,0,0);
+itemArr[12] = new item(150,0,0);
+itemArr[13] = new item(140,0,0);
+itemArr[14] = new item(200,0,0);
+itemArr[15] = new item(80,0,0);
+itemArr[16] = new item(120,0,0);
+sessionStorage[itemArr] = JSON.stringify(itemArr);
 function add()
 {
-
 	for(var i = 0; i < 17; i++)
 	{
-		if(document.getElementById(i).value > 0)
-		{
-			quantity += parseInt(document.getElementById(i).value);
-			if(i == 0 && document.getElementById(i).value != null )
-			{
-			   price = 450 * quantity;
-			   amount += price;
-			   document.getElementById(i).value = 0;
-			   quantity = 0;
-			}
-			else if(i == 1 && document.getElementById(i).value != null )
-			{
-				price = 600 * quantity;
-				amount += price;
-				quantity = 0;
-				document.getElementById(i).value = 0;
-			}
-			else if(i == 2 && document.getElementById(i).value != null )
-			{
-				price = 500 * quantity;
-			  amount += price;
-				quantity = 0;
-				document.getElementById(i).value = 0;
-			}
-			else if(i == 3 && document.getElementById(i).value != null )
-			{
-				price = 800 * quantity;
-				amount += price;
-				quantity = 0;
-				document.getElementById(i).value = 0;
-			}
-			else if(i == 4 && document.getElementById(i).value != null )
-			{
-				price = 250 * quantity;
-				amount += price;
-				quantity = 0;
-				document.getElementById(i).value = 0;
-			}
-			if(i == 5 && document.getElementById(i).value != null )
-			{
-				price = 90 * quantity;
-			  amount += price;
-				quantity = 0;
-				document.getElementById(i).value = 0;
-			}
-
-			else if(i == 6 && document.getElementById(i).value != null )
-			{
-				price = 80 * quantity;
-				amount += price;
-				quantity = 0;
-				document.getElementById(i).value = 0;
-			}
-			else if(i == 7 && document.getElementById(i).value != null )
-			{
-				price = 200 * quantity;
-				amount += price;
-				quantity = 0;
-				document.getElementById(i).value = 0;
-			}
-			else if(i == 8 && document.getElementById(i).value != null )
-			{
-				price = 180 * quantity;
-				amount += price;
-				quantity = 0;
-				document.getElementById(i).value = 0;
-			}
-			else if(i == 9 && document.getElementById(i).value != null )
-			{
-				price = 230 * quantity;
-				amount += price;
-				quantity = 0;
-				document.getElementById(i).value = 0;
-			}
-			else if(i == 10 && document.getElementById(i).value != null )
-			{
-				price = 300 * quantity;
-				amount += price;
-				quantity = 0;
-				document.getElementById(i).value = 0;
-			}
-			else if(i == 11 && document.getElementById(i).value != null )
-			{
-				price = 350 * quantity;
-				amount += price;
-				quantity = 0;
-				document.getElementById(i).value = 0;
-			}
-			else if(i == 12 && document.getElementById(i).value != null )
-			{
-				price = 300 * quantity;
-				amount += price;
-				quantity = 0;
-				document.getElementById(i).value = 0;
-			}
-			else if(i == 13 && document.getElementById(i).value != null )
-			{
-				price = 150 * quantity;
-				amount += price;
-				quantity = 0;
-				document.getElementById(i).value = 0;
-			}
-			else if(i == 14 && document.getElementById(i).value != null )
-			{
-				price = 140 * quantity;
-				amount += price;
-				quantity = 0;
-				document.getElementById(i).value = 0;
-			}
-			else if(i == 15 && document.getElementById(i).value != null )
-			{
-				price = 200 * quantity;
-				amount += price;
-				quantity = 0;
-				document.getElementById(i).value = 0;
-			}
-			else if(i == 16 && document.getElementById(i).value != null )
-			{
-				price = 80 * quantity;
-				amount += price;
-				quantity = 0;
-				document.getElementById(i).value = 0;
-			}
-			else if(i == 17 && document.getElementById(i).value != null )
-			{
-				price = 120 * quantity;
-				amount += price;
-				quantity = 0;
-				document.getElementById(i).value = 0;
-			}
+      if(document.getElementById(i) != 0)
+      {
+         itemArr[i].quan = document.getElementById(i).value;
+         sessionStorage[itemArr[i].totalQuan] += JSON.stringify(parseInt(itemArr[i].totalQuan + itemArr[i].quan));
+         amount += itemArr[i].price * itemArr[i].quan;
+         document.getElementById(i).value = "";
+      }
 			if(previous > 0)
 			{
 				sessionStorage.cost = previous + amount;
 				document.getElementById("cost").innerHTML = "<strong>Current amount in cart: €" + sessionStorage.cost + "</strong>";
 			}
-			else{
-				sessionStorage.cost = amount;
-					document.getElementById("cost").innerHTML = "<strong>Current amount in cart: €" + sessionStorage.cost + "</strong>";
+			else
+      {
+				 sessionStorage.cost = amount;
+				 document.getElementById("cost").innerHTML = "<strong>Current amount in cart: €" + sessionStorage.cost + "</strong>";
 			}
-		}
-	}
 
-}
+    }
+  }
